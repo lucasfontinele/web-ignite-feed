4b90d28dafd1adfc97d16da2e4977109f4b3f3aa
Lucas <lfontinele07@gmail.com>
2023-02-01 14:44:32 -0300

    feat: implements user card component

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,51 @@
+import { FaEdit } from 'react-icons/fa'
+import { UserCardProps } from '.'
+import { Button, StyleVariant } from '../Button'
+import styles from './UserCard.module.css'
+
+export const UserCard = ({
+  name,
+  bio,
+  coverUrl,
+  pictureUrl,
+  profileUrl
+}: UserCardProps) => {
+  return (
+    <article
+      data-testid="user-card"
+      role="article"
+      className={styles.container}
+    >
+      <img
+        src={coverUrl}
+        data-testid="user-card-cover"
+        className={styles.cover}
+      />
+      <img
+        src={pictureUrl}
+        data-testid="user-card-picture"
+        className={styles.picture}
+      />
+
+      <div className={styles.content}>
+        <p data-testid="user-card-name" className={styles.name}>
+          {name}
+        </p>
+        <p data-testid="user-card-bio" className={styles.bio}>
+          {bio}
+        </p>
+      </div>
+
+      <div className={styles.divider} />
+
+      <div className={styles.buttonContainer}>
+        <Button
+          icon={<FaEdit size={20} />}
+          variant={StyleVariant.outlined}
+          title="Editar seu perfil"
+          data-testid="user-card-profileUrl"
+        />
+      </div>
+    </article>
+  )
+}
