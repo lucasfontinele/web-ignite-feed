860ffbeb9fcc656fc99abe53163455b4b053ae9b
Lucas <lfontinele07@gmail.com>
2023-02-06 12:02:49 -0300

    chore: improvements on profile picture component

--- src/components/ProfilePicture/ProfilePicture.stories.tsx
+++ src/components/ProfilePicture/ProfilePicture.stories.tsx
@@ -9,5 +9,13 @@ export default {
 export const Default: Story<ProfilePictureProps> = (args: ProfilePictureProps) => <ProfilePicture {...args} />
 
 Default.args = {
-  src: "https://images.mubicdn.net/images/cast_member/9020/cache-148043-1465730065/image-w856.jpg"
+  src: "https://images.mubicdn.net/images/cast_member/9020/cache-148043-1465730065/image-w856.jpg",
+  variant: 'normal'
+}
+
+export const Highlighted: Story<ProfilePictureProps> = (args: ProfilePictureProps) => <ProfilePicture {...args} />
+
+Highlighted.args = {
+  src: "https://images.mubicdn.net/images/cast_member/9020/cache-148043-1465730065/image-w856.jpg",
+  variant: 'highlight',
 }
